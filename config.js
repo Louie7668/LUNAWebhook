ee205664fa6d8fa4140f3c415d4b6e38063dece1
Spiderr <evancall08ty@gmail.com>
2023-06-29 21:12:14 +0100

    Author and Name changed

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
+// LUNA Studios
+
 const webhook = 'https://discord.com/api/webhooks/1096202918731976774/GwsTIrw1Xjmp_c3JefInR5J6Sidcyeb37QmSRSvxK8ubwdrFEPFEXV1BsfiiV19kbjNK' //PUT YOUR WEBHOOK HEREe
 const server_port = 30145;  // Put Your Port Here
 
